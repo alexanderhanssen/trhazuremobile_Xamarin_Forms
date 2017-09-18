6b9d825d16c54749508b312371863fb5e33b748b
alexanderhanssen <alexhsn1@gmail.com>
2017-09-18 16:17:03 +0200

    Initial commit

---
+++ trhazuremobile/App.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Xamarin.Forms;
+
+namespace trhazuremobile
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new TodoList();
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+		    MobileCenter.Start("android=ea2c7d88-e7ea-42f1-8906-c9537dbf18b6;",
+		        typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ trhazuremobile/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace trhazuremobile
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://trhazuremobile.azurewebsites.net";
+	}
+}
+
