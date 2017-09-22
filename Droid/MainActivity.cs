aa802158b21fb3546cd7d57a73e1f25872356a40
alexanderhanssen <alexhsn1@gmail.com>
2017-09-22 14:12:50 +0200

    Adds gcm

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-
+using Gcm.Client;
 using Microsoft.WindowsAzure.MobileServices;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -22,9 +16,24 @@ namespace trhazuremobile.Droid
 		Theme = "@android:style/Theme.Holo.Light")]
 	public class MainActivity : FormsApplicationActivity
 	{
-		protected override void OnCreate (Bundle bundle)
+
+	    // Create a new instance field for this activity.
+	    static MainActivity instance = null;
+
+	    // Return the current activity instance.
+	    public static MainActivity CurrentActivity
+	    {
+	        get
+	        {
+	            return instance;
+	        }
+	    }
+
+        protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
+		    // Set the current instance of MainActivity.
+		    instance = this;
+            base.OnCreate (bundle);
 
 			// Initialize Azure Mobile Apps
 			CurrentPlatform.Init();
@@ -34,7 +43,34 @@ namespace trhazuremobile.Droid
 
 			// Load the main application
 			LoadApplication (new App ());
-		}
-	}
+		    try
+		    {
+		        // Check to ensure everything's set up right
+		        GcmClient.CheckDevice(this);
+		        GcmClient.CheckManifest(this);
+
+		        // Register for push notifications
+		        System.Diagnostics.Debug.WriteLine("Registering...");
+		        GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+		    }
+		    catch (Java.Net.MalformedURLException)
+		    {
+		        CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+		    }
+		    catch (Exception e)
+		    {
+		        CreateAndShowDialog(e.Message, "Error");
+		    }
+        }
+
+	    private void CreateAndShowDialog(String message, String title)
+	    {
+	        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+	        builder.SetMessage(message);
+	        builder.SetTitle(title);
+	        builder.Create().Show();
+	    }
+    }
 }
 
