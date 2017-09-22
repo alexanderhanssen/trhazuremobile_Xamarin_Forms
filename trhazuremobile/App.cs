d9b44383c558d3c61277bb4082aa409af63df12e
Rino Andre Johnsen <rinoandrejohnsen@gmail.com>
2017-09-22 16:13:26 +0200

    Push!

--- trhazuremobile/App.cs
+++ trhazuremobile/App.cs
@@ -15,14 +15,38 @@ namespace trhazuremobile
 			MainPage = new TodoList();
 		}
 
-		protected override void OnStart ()
+		protected override async void OnStart ()
 		{
             // Handle when your app starts
-		    MobileCenter.Start("android=ea2c7d88-e7ea-42f1-8906-c9537dbf18b6;",
+		    MobileCenter.Start(
+                "uwp=df13eb47-6f23-4e8f-841f-8c9838cef8ff;" +
+                "android=c8972ad8-a269-4349-81c2-35695fe0882e;",
 		        typeof(Analytics), typeof(Crashes), typeof(Push));
+
+            Push.PushNotificationReceived += (sender, e) => {
+
+                // Add the notification message and title to the message
+                var summary = $"Push notification received:" +
+                                    $"\n\tNotification title: {e.Title}" +
+                                    $"\n\tMessage: {e.Message}";
+
+                // If there is custom data associated with the notification,
+                // print the entries
+                if (e.CustomData != null)
+                {
+                    summary += "\n\tCustom data:\n";
+                    foreach (var key in e.CustomData.Keys)
+                    {
+                        summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                    }
+                }
+
+                // Send the notification summary to debug output
+                System.Diagnostics.Debug.WriteLine(summary);
+            };
         }
 
-		protected override void OnSleep ()
+        protected override void OnSleep ()
 		{
 			// Handle when your app sleeps
 		}
